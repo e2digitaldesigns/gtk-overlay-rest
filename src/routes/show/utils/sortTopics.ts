8fbd616f2843ae83887033702c3ad80d5ba2524b
Casana E. Bey <info@expresslayouts.com>
2023-03-28 14:56:08 -0400

    Fix topic sorting bug

--- src/routes/show/utils/sortTopics.ts
+++ src/routes/show/utils/sortTopics.ts
@@ -1,20 +1,51 @@
 const _sortBy = require("lodash/sortBy");
 import { IEpisodeTopic } from "../../../models/episodes.model";
 
-export const sortTopics = (topics: IEpisodeTopic) => {
+// export const sortTopics = (topics: IEpisodeTopic) => {
+//   const initSort = _sortBy(topics, "order");
+//   let finalSort: IEpisodeTopic[] = [];
+
+//   for (let i = 0; i < initSort.length; i++) {
+//     if (finalSort.includes(initSort[i])) {
+//     } else if (initSort[i].isParent === true) {
+//       finalSort.push(initSort[i]);
+//       let children = initSort.filter(
+//         (topic: IEpisodeTopic) =>
+//           topic.isChild === true && topic.parentId === initSort[i]._id
+//       );
+
+//       finalSort = finalSort.concat(children);
+//     } else {
+//       finalSort.push(initSort[i]);
+//     }
+//   }
+
+//   return finalSort;
+// };
+
+export const sortTopics = (topics: IEpisodeTopic[]) => {
   const initSort = _sortBy(topics, "order");
   let finalSort: IEpisodeTopic[] = [];
 
   for (let i = 0; i < initSort.length; i++) {
-    if (finalSort.includes(initSort[i])) {
+    if (initSort[i].isChild === true) {
+      console.log(initSort[i].name, "is a child");
+    } else if (finalSort.includes(initSort[i])) {
+      console.log(initSort[i].name, "already included");
     } else if (initSort[i].isParent === true) {
       finalSort.push(initSort[i]);
-      let children = initSort.filter(
-        (topic: IEpisodeTopic) =>
-          topic.isChild === true && topic.parentId === initSort[i]._id
-      );
+      console.log(initSort[i].name, "is a parent");
 
-      finalSort = finalSort.concat(children);
+      initSort.map((topic: IEpisodeTopic) => {
+        if (
+          topic.parentId === initSort[i]._id &&
+          topic.isChild === true &&
+          !finalSort.includes(topic)
+        ) {
+          finalSort.push(topic);
+          console.log(topic.name, "is a child");
+        }
+      });
     } else {
       finalSort.push(initSort[i]);
     }
