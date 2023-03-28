8eddd38417d46851cee571ff7b1a8e9bf3506b03
Casana E. Bey <info@expresslayouts.com>
2023-03-28 15:32:50 -0400

    address id issue

--- src/routes/show/utils/sortTopics.ts
+++ src/routes/show/utils/sortTopics.ts
@@ -29,7 +29,7 @@ export const sortTopics = (topics: IEpisodeTopic[]) => {
 
   for (let i = 0; i < initSort.length; i++) {
     if (initSort[i].isChild === true) {
-      console.log(initSort[i].name, "is a child");
+      console.log(initSort[i].name, "is a child - top level");
     } else if (finalSort.includes(initSort[i])) {
       console.log(initSort[i].name, "already included");
     } else if (initSort[i].isParent === true) {
@@ -37,13 +37,36 @@ export const sortTopics = (topics: IEpisodeTopic[]) => {
       console.log(initSort[i].name, "is a parent");
 
       initSort.map((topic: IEpisodeTopic) => {
-        if (
-          topic.parentId === initSort[i]._id &&
+        console.log(40, "cid:", initSort[i]._id, initSort[i].name);
+        console.log(41, "pid:", topic.parentId);
+        console.log(42, "isChild:", topic.isChild);
+        console.log(
+          43,
+          "match:",
+          String(topic.parentId) === String(initSort[i]._id)
+        );
+
+        console.log(49, "need false, is included:", finalSort.includes(topic));
+
+        const shouldAdd =
+          String(topic.parentId) === String(initSort[i]._id) &&
+          topic.isChild === true;
+
+        console.log(55, "shouldAdd:", shouldAdd);
+
+        const shouldAdd2 =
+          String(topic.parentId) === String(initSort[i]._id) &&
           topic.isChild === true &&
-          !finalSort.includes(topic)
+          !finalSort.includes(topic);
+
+        console.log(62, "shouldAdd:", shouldAdd2);
+
+        if (
+          String(topic.parentId) === String(initSort[i]._id) &&
+          topic.isChild === true
         ) {
           finalSort.push(topic);
-          console.log(topic.name, "is a child");
+          console.log(topic.name, "is a child AND is being added");
         }
       });
     } else {
