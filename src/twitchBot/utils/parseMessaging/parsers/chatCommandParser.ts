8780d5cd4fe3fe223697b1eb245b8a80c3c6e40c
Casana E. Bey <info@expresslayouts.com>
2023-12-09 17:28:58 -0500

    Add t-or-f

--- src/twitchBot/utils/parseMessaging/parsers/chatCommandParser.ts
+++ src/twitchBot/utils/parseMessaging/parsers/chatCommandParser.ts
@@ -14,7 +14,7 @@ export async function chatCommandParser(
 
   switch (command) {
     case "!gtk":
-      client.action(channel, "GamerToolkit Test Command Local");
+      client.action(channel, "GamerToolkit Test Command");
       break;
 
     case "!res":
@@ -33,23 +33,31 @@ export async function chatCommandParser(
     case "!d1":
     case "!d2":
     case "!d3":
+    case "!d4":
     case "!v1":
     case "!v2":
     case "!v3":
+    case "!v4":
       await chatCommands.overlayVoting(command, tags.username, channel, socket);
       break;
 
     case "!sv1":
     case "!sv2":
     case "!sv3":
+    case "!sv4":
       if (!tags.subscriber) {
         console.log("not a sub");
         client.action(
           channel,
-          `@${tags.username}, you must be a subscriber use super votes!`
+          `@${tags.username}, you must be a subscriber to use super votes!`
         );
         return;
       }
+      await chatCommands.overlayVoting(command, tags.username, channel, socket);
+      break;
+
+    case "!true":
+    case "!false":
       await chatCommands.overlayVoting(command, tags.username, channel, socket);
       break;
 
