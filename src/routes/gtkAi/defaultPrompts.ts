f1002e98118da3e90f5fee829d3e153793dce940
Casana E. Bey <info@expresslayouts.com>
2023-12-09 23:09:10 -0500

    New AI Routes

---
+++ src/routes/gtkAi/defaultPrompts.ts
@@ -0,0 +1,24 @@
+export const prompts = {
+  podcastData: `You are a digital and social marketing media genius.
+
+  Provide a catchy and appealing podcast topic title that encapsulates the essence of the given text.
+
+  Provide a witty and attention grabing podcast topic description. The description shoould be 40 to 50 characters long and be written at a 8th grade reading level.
+
+  DO NOT USE Quotes, or the words Discover in the title or description.
+  `,
+
+  youtubeTitle:
+    "You are a digital and social marketing media genius. Propose a catchy and appealing youtube video title that encapsulates the essence of",
+
+  youtubeDescription:
+    "Engaging Description: You are a digital and social marketing media genius. Provide a concise 40 - 50 character 6th grade reading level summary of the given text as if you were preparing an introduction for a youtube video description.",
+
+  youtubeMetaData: `You are a digital and social marketing media genius.
+
+    Provide a catchy and appealing youtube video title that encapsulates the essence of the given text.
+
+    Provide a witty and attention grabing youtube video description. The description shoould be 40 to 50 characters long and be written at a 8th grade reading level.
+    
+    Provide hashtags that are relevant to the given text and will help the video get discovered by the right audience.`
+};
--- src/routes/gtkAi/gtkAi.ts
+++ src/routes/gtkAi/gtkAi.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from "express";
 import OpenAI from "openai";
+import { prompts } from "./defaultPrompts";
 
 const router = express.Router();
+
+enum AIType {
+  PodcastData = "podcastData",
+  ThumbnailImage = "thumbnailImage",
+  YoutubeData = "youtubeData"
+}
 
 router.post("/img", async (req: Request, res: Response) => {
   const openai = new OpenAI();
@@ -27,9 +34,7 @@ router.post("/txt", async (req: Request, res: Response) => {
 
   try {
     const defaultPrompt =
-      aiType === "title"
-        ? "Engaging Title: Propose a catchy and appealing title that encapsulates the essence of"
-        : "provide a concise 40 - 50 character 6th grade reading level summary of the given text as if you were preparing an introduction for a podcast topic";
+      aiType === "title" ? prompts.youtubeTitle : prompts.youtubeDescription;
     const result = await openai.chat.completions.create({
       messages: [
         {
@@ -48,6 +53,54 @@ router.post("/txt", async (req: Request, res: Response) => {
 
 router.get("/", (req: Request, res: Response) => {
   res.status(200).send("GTK AI");
+});
+
+router.post("/gtk-assistant", async (req: Request, res: Response) => {
+  const openai = new OpenAI();
+  const { aiPrompt, aiType } = req.body;
+
+  try {
+    if (aiType === (AIType.YoutubeData as string)) {
+      const result = await openai.chat.completions.create({
+        messages: [
+          {
+            role: "system",
+            content: `${prompts.youtubeMetaData} ${aiPrompt}`
+          }
+        ],
+        model: "gpt-3.5-turbo"
+      });
+
+      res.status(200).json(result?.choices?.[0]?.message?.content);
+    }
+
+    if (aiType === (AIType.PodcastData as string)) {
+      const result = await openai.chat.completions.create({
+        messages: [
+          {
+            role: "system",
+            content: `${prompts.podcastData} ${aiPrompt}`
+          }
+        ],
+        model: "gpt-3.5-turbo"
+      });
+
+      res.status(200).json(result?.choices?.[0]?.message?.content);
+    }
+
+    if (aiType === (AIType.ThumbnailImage as string)) {
+      const response = await openai.images.generate({
+        model: "dall-e-3",
+        prompt: aiPrompt,
+        n: 1,
+        size: "1792x1024"
+      });
+
+      res.status(200).json(response?.data?.[0].url);
+    }
+  } catch (error: unknown) {
+    res.status(404).send(error);
+  }
 });
 
 export const gtkAi = router;
